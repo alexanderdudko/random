d9dd287b36ca36b95692e8caa85501eed03c75b5
Alexander <aleksandrsdudko@gmail.com>
2018-11-10 17:20:48 +0200

    Added one more uniform random source CryptoUniformRandomSource which is based on cryptographic random generator.

---
+++ Generator/RandomGenerator/Uniform/CryptoUniformRandomSource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Generator.RandomGenerator.Uniform
+{
+    public class CryptoUniformRandomSource : IUniformDistributionGenerator
+    {
+        // http://eimagine.com/how-to-generate-better-random-numbers-in-c-net-2/
+
+        private RandomNumberGenerator mRandom = RandomNumberGenerator.Create();
+
+        public double Next()
+        {
+            byte[] b = new byte[4];
+            mRandom.GetBytes(b);
+            return (double)BitConverter.ToUInt32(b, 0) / UInt32.MaxValue;
+        }
+    }
+}
