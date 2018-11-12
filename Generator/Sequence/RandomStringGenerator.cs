aa83cf5317bd6e5664b78828e7dccf126347fd61
Alexander <aleksandrsdudko@gmail.com>
2018-11-12 18:33:45 +0200

    Added random string generators.

---
+++ Generator/Sequence/RandomStringGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Generator.Distribution;
+using Generator.RandomGenerator;
+using Generator.RandomGenerator.Discrete;
+using Generator.RandomGenerator.Uniform;
+
+namespace Generator.Sequence
+{
+    public class RandomStringGenerator : SequenceGenerator<char>
+    {
+        public RandomStringGenerator(string chars = "abcdefghijklmnopqrstuvwxyz") : base(new DiscreteFiniteSetValueGenerator<char>(new EmpiricalDataDistribution<char>(chars.ToCharArray()), new SystemUniformRandomSource()))
+        { }
+
+        public string GetString(int length)
+        {
+            return new string(Generate(length).ToArray());
+        }
+    }
+}
---
+++ Generator/Sequence/UniqueRandomStringGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generator.Sequence
+{
+    public class UniqueRandomStringGenerator
+    {
+        private int mLength;
+        private RandomStringGenerator mGenerator = new RandomStringGenerator();
+        private HashSet<string> mHistory = new HashSet<string>();
+
+        public UniqueRandomStringGenerator(int length)
+        {
+            mLength = length;
+        }
+
+        public string GetUniqueString(int maxTriesCount = 1000)
+        {
+            string next;
+            int triesCount = 0;
+            do
+            {
+                next = mGenerator.GetString(mLength);
+                triesCount++;
+            } while (mHistory.Contains(next) || triesCount > maxTriesCount);
+            mHistory.Add(next);
+
+            if (triesCount > maxTriesCount)
+                throw new Exception("Reached max tries count while generating unique string");
+
+            return next;
+        }
+
+        public void ClearHistory()
+        {
+            mHistory.Clear();
+        }
+    }
+}
