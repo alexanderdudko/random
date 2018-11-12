b8fdfe12582ce915fc2aa535bf09ef4cc4098e0f
Alexander <aleksandrsdudko@gmail.com>
2018-11-12 18:43:51 +0200

    Changes in main program code. * Added logging progress to log file * Added all generated files info special file * Removed unused temporary code

--- GeneratorCLI/Program.cs
+++ GeneratorCLI/Program.cs
@@ -13,129 +13,84 @@ namespace GeneratorCLI
 {
     class Program
     {
+        static string mLogFilePath;
+
         static void Main(string[] args)
         {
             string basePath = @"H:\projects\data\random";
             var path = Directory.CreateDirectory(Path.Combine(basePath, DateTime.Now.ToString("yyyyMMdd-HHmmss"))).FullName;
+            mLogFilePath = Path.Combine(path, "log.txt");
+
             int fileSize = 1024 * 1024; // 1MB
-            int numberOfFiles = 16;
+            int numberOfFiles = 200;
 
             GenerateFiles(path, fileSize, numberOfFiles);
         }
 
         private static void GenerateFiles(string path, int fileSize, int numberOfFiles)
         {
+            var logFile = new LogFile<RandomFilesLogRecord>(Path.Combine(path, "FilesInfo.txt"), new RandomFilesLogRecordSerializer());
+            var logRecords = new List<RandomFilesLogRecord>();
+
+            var nameGenerator = new UniqueRandomStringGenerator(4);
+
             int n = 256;
-            var d = new GeometricProgressionDistribution<byte>(Enumerable.Range(0, n).Select(x => (byte)x), 1);
+            var distribution = new GeometricProgressionDistribution<byte>(Enumerable.Range(0, n).Select(x => (byte)x), 1);
             double k = 1;
             for (int i = 0; i < numberOfFiles; i++)
             {
                 double progression = Math.Pow(k, 1.0 / (n - 1));
-                d.SetProgressionRate(progression);
+                distribution.SetProgressionRate(progression);
 
-                string filename = $"{i}.data";
-                Console.WriteLine($"Generating file {filename}.");
-                GenerateFile(d, Path.Combine(path, filename), fileSize);
+                string filename = $"{nameGenerator.GetUniqueString()}.data";
+                LogMessage($"Generating file [{i + 1}/{numberOfFiles}]: Filename {filename}.");
+                byte[] data = GenerateData(distribution, fileSize);
+                File.WriteAllBytes(Path.Combine(path, filename), data);
+                var logRecord = new RandomFilesLogRecord()
+                {
+                    FileName = filename,
+                    ProgressionRate = progression,
+                    GeneratorEntropy = EntropyCalculator.CalculateForDistribution(distribution),
+                    DataEntropy = EntropyCalculator.CalculateForData(data)
+                };
+                logRecords.Add(logRecord);
 
                 k *= 2;
             }
+
+            logFile.WriteAllRecords(logRecords);
         }
 
-        private static void GenerateFile(IDiscreteValueDistribution<byte> distribution, string filename, int size)
+        private static byte[] GenerateData(IDiscreteValueDistribution<byte> distribution, int size, double entropyDiff = 0.001)
         {
             double generatorEntropy = EntropyCalculator.CalculateForDistribution(distribution);
-            Console.WriteLine($"  Generator entropy {generatorEntropy:N5}");
+            LogMessage($"  Generator entropy {generatorEntropy:N5}");
 
-            var randomSource = new DiscreteFiniteSetValueGenerator<byte>(distribution, new SystemUniformRandomSource());
+            //var uniformRandomSource = new SystemUniformRandomSource();
+            var uniformRandomSource = new CryptoUniformRandomSource();
+            var randomSource = new DiscreteFiniteSetValueGenerator<byte>(distribution, uniformRandomSource);
             var sg = new SequenceGenerator<byte>(randomSource);
             byte[] bytes;
+            double empiricalDataEntropy = 0;
             double difference;
             do
             {
                 bytes = sg.Generate(size).ToArray();
 
-                double empiricalDataEntropy = EntropyCalculator.CalculateForData(bytes);
+                empiricalDataEntropy = EntropyCalculator.CalculateForData(bytes);
                 difference = Math.Abs(generatorEntropy - empiricalDataEntropy);
-                Console.WriteLine($"  Generated empirical data with entropy {empiricalDataEntropy:N5}. Difference: {difference:N5}.");
-            } while (difference > 0.001);
-            File.WriteAllBytes(filename, bytes);
+                LogMessage($"  Generated empirical data with entropy {empiricalDataEntropy:N5}. Difference: {difference:N5}.");
+            } while (difference > entropyDiff);
+
+            return bytes;
         }
 
-        private static void Test_EightBitProgressionDistGenerator_Entropy()
+        private static void LogMessage(string message)
         {
-            int n = 256;
-            var d = new GeometricProgressionDistribution<int>(Enumerable.Range(0, n), 1);
-            double k = 1;
-            for (int i = 0; i < 200; i++)
-            {
-                double progression = Math.Pow(k, 1.0 / (n - 1));
-                d.SetProgressionRate(progression);
-                Console.WriteLine($"{EntropyCalculator.CalculateForDistribution(d)}");
-                k *= 2;
-            }
+            Console.WriteLine(message);
+            File.AppendAllLines(mLogFilePath, new string[] { $"[{DateTime.Now:yyyyMMdd-HHmmss}] {message}" });
         }
 
-        private static void Test_EightSymbolsProgressionDistribution_Entropy()
-        {
-            var d = new GeometricProgressionDistribution<string>(
-                new string[] { "A", "B", "C", "D", "E", "F", "G", "H" },
-                2
-            );
-            foreach (var item in d.Values)
-            {
-                Console.WriteLine($"Probability({item}) = {d.ValueProbability(item)}");
-            }
-            Console.WriteLine($"Entropy = {EntropyCalculator.CalculateForDistribution(d)}");
-        }
-
-        private static void Test_ThreeSymbolDistribution_OutputProbability()
-        {
-            var distribution = new ThreeSymbolDistribution(0.5, 1.0 / 3.0);
-            var randomSource = new DiscreteFiniteSetValueGenerator<string>(distribution, new SystemUniformRandomSource());
-            var sg = new SequenceGenerator<string>(randomSource);
-            foreach (var item in sg.Generate(10))
-            {
-                Console.WriteLine(item);
-            }
-            var test = sg.Generate(1000000).ToArray();
-            Func<string, double> pFunc = v => (double)test.Count(x => x == v) / test.Length;
-            foreach (var item in distribution.Values)
-            {
-                Console.WriteLine($"Probability({item}) = {pFunc(item):N5}");
-            }
-        }
     }
 
-    class ThreeSymbolDistribution : IDiscreteValueDistribution<string>
-    {
-        public const string Alpha = "Alpha";
-        public const string Beta = "Beta";
-        public const string Gamma = "Gamma";
-
-        public IEnumerable<string> Values => new string[] { Alpha, Beta, Gamma };
-
-        private double mProbabilityAlpha;
-        private double mProbabilityBeta;
-
-        public ThreeSymbolDistribution(double probabilityAlpha, double probabilityBeta)
-        {
-            mProbabilityAlpha = probabilityAlpha;
-            mProbabilityBeta = probabilityBeta;
-        }
-
-        public double ValueProbability(string value)
-        {
-            switch (value)
-            {
-                case Alpha:
-                    return mProbabilityAlpha;
-                case Beta:
-                    return mProbabilityBeta;
-                case Gamma:
-                    return 1 - mProbabilityAlpha - mProbabilityBeta;
-                default:
-                    throw new ArgumentOutOfRangeException($"Value \"{value}\" is not supported.");
-            }
-        }
-    }
 }
