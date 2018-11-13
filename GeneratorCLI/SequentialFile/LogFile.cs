72711b08d5d46cd193dc7eb7e709e75d7562e0d9
Alexander <aleksandrsdudko@gmail.com>
2018-11-13 08:55:18 +0200

    Added missing files related to writing sequential data to file

---
+++ GeneratorCLI/SequentialFile/LogFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GeneratorCLI
+{
+    class LogFile<TRecord>
+    {
+        private string mPath;
+        private ILogFileRecordSerializer<TRecord> mSerializer;
+
+        public LogFile(string path, ILogFileRecordSerializer<TRecord> serializer)
+        {
+            mPath = path;
+            mSerializer = serializer;
+        }
+
+        public void WriteAllRecords(IEnumerable<TRecord> records)
+        {
+            File.WriteAllLines(mPath, records.Select(r => mSerializer.Serialize(r)));
+        }
+
+        public IEnumerable<TRecord> ReadAllRecords()
+        {
+            return File.ReadAllLines(mPath).Select(t => mSerializer.Deserialize(t));
+        }
+    }
+
+    interface ILogFileRecordSerializer<TRecord>
+    {
+        string Serialize(TRecord record);
+        TRecord Deserialize(string text);
+    }
+}
---
+++ GeneratorCLI/SequentialFile/RandomFilesLogRecord.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneratorCLI
+{
+    class RandomFilesLogRecord
+    {
+        public string FileName { get; set; }
+        public double ProgressionRate { get; set; }
+        public double GeneratorEntropy { get; set; }
+        public double DataEntropy { get; set; }
+    }
+
+    class RandomFilesLogRecordSerializer : ILogFileRecordSerializer<RandomFilesLogRecord>
+    {
+        private const char SEPARATOR = '\t';
+
+        public RandomFilesLogRecord Deserialize(string text)
+        {
+            var parts = text.Split(SEPARATOR);
+            return new RandomFilesLogRecord()
+            {
+                FileName = parts[0],
+                ProgressionRate = double.Parse(parts[1]),
+                GeneratorEntropy = double.Parse(parts[2]),
+                DataEntropy = double.Parse(parts[3])
+            };
+        }
+
+        public string Serialize(RandomFilesLogRecord record)
+        {
+            return string.Join(SEPARATOR,
+                record.FileName,
+                record.ProgressionRate,
+                record.GeneratorEntropy,
+                record.DataEntropy
+            );
+        }
+    }
+}
