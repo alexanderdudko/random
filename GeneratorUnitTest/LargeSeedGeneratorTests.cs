993f3cc1ed92365196bc83445c3a409f5f5db92c
Alexander <aleksandrsdudko@gmail.com>
2018-11-22 12:15:35 +0200

    Added one more unite test for large sequene genertor to ensure that entire seed is taken into account

--- GeneratorUnitTest/LargeSeedGeneratorTests.cs
+++ GeneratorUnitTest/LargeSeedGeneratorTests.cs
@@ -18,9 +18,10 @@ namespace GeneratorUnitTest
             var generator2 = new LargeSeedUniformRandomSource();
             generator1.SetSeed(seed1);
             generator2.SetSeed(seed2);
+            int numberOfValues = 100;
 
-            var sequence1 = Enumerable.Range(0, 100).Select(x => generator1.Next()).ToArray();
-            var sequence2 = Enumerable.Range(0, 100).Select(x => generator2.Next()).ToArray();
+            var sequence1 = Enumerable.Range(0, numberOfValues).Select(x => generator1.Next()).ToArray();
+            var sequence2 = Enumerable.Range(0, numberOfValues).Select(x => generator2.Next()).ToArray();
 
             Assert.NotEqual(seed1, seed2);
             Assert.NotEqual(sequence1, sequence2);
@@ -34,11 +35,44 @@ namespace GeneratorUnitTest
             var generator2 = new LargeSeedUniformRandomSource();
             generator1.SetSeed(seed);
             generator2.SetSeed(seed);
+            int numberOfValues = 100;
 
-            var sequence1 = Enumerable.Range(0, 100).Select(x => generator1.Next()).ToArray();
-            var sequence2 = Enumerable.Range(0, 100).Select(x => generator2.Next()).ToArray();
+            var sequence1 = Enumerable.Range(0, numberOfValues).Select(x => generator1.Next()).ToArray();
+            var sequence2 = Enumerable.Range(0, numberOfValues).Select(x => generator2.Next()).ToArray();
 
             Assert.Equal(sequence1, sequence2);
+        }
+
+        [Fact]
+        public void AllBytesOfTheSeedAreTakenIntoAccountTest()
+        {
+            byte mutation = 47;
+            int byteNumber = 0;
+
+            byte[] originalSeed = Guid.NewGuid().ToByteArray();
+            byte[] seed1 = originalSeed.ToArray();
+            byte[] seed2 = originalSeed.ToArray();
+            seed1[0 + byteNumber] = (byte)((seed1[0 + byteNumber] + mutation) % 256);
+            seed2[4 + byteNumber] = (byte)((seed2[4 + byteNumber] + mutation) % 256);
+            var originalGenerator = new LargeSeedUniformRandomSource();
+            var generator1 = new LargeSeedUniformRandomSource();
+            var generator2 = new LargeSeedUniformRandomSource();
+            originalGenerator.SetSeed(originalSeed);
+            generator1.SetSeed(seed1);
+            generator2.SetSeed(seed2);
+            int numberOfValues = 100;
+
+            var original = Enumerable.Range(0, numberOfValues).Select(x => originalGenerator.Next()).ToArray();
+            var sequence1 = Enumerable.Range(0, numberOfValues).Select(x => generator1.Next()).ToArray();
+            var sequence2 = Enumerable.Range(0, numberOfValues).Select(x => generator2.Next()).ToArray();
+
+            Assert.NotEqual(originalSeed, seed1);
+            Assert.NotEqual(originalSeed, seed2);
+            Assert.NotEqual(seed1, seed2);
+
+            Assert.NotEqual(original, sequence1);
+            Assert.NotEqual(original, sequence2);
+            Assert.NotEqual(sequence1, sequence2);
         }
     }
 }
