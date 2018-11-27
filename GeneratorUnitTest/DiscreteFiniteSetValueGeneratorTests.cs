844b1a6a5d87dfde5b69a568824ad6a34213d649
Alexander <aleksandrsdudko@gmail.com>
2018-11-27 14:33:06 +0200

    Added unit tests for discrete finite set value generator and its output entropy calculation

---
+++ GeneratorUnitTest/DiscreteFiniteSetValueGeneratorTests.cs
@@ -0,0 +1,48 @@
+﻿using Generator.Distribution;
+using Generator.Distribution.Entropy;
+using Generator.RandomGenerator.Continuous;
+using Generator.RandomGenerator.Discrete;
+using Generator.Sequence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace GeneratorUnitTest
+{
+    public class DiscreteFiniteSetValueGeneratorTests
+    {
+        [Fact]
+        public void OutputEmpiricalEntropyEqualToTheoreticalEntropyTest()
+        {
+            int count = 1000000;
+            var data = new char[] { 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'D' };
+            var distribution = new EmpiricalDataDistribution<char>(data);
+            var randomSource = new DiscreteFiniteSetValueGenerator<char>(distribution, new SystemUniformRandomSource());
+            var sg = new SequenceGenerator<char>(randomSource);
+            var generatedData = sg.Generate(count);
+
+            double theoreticalEntropy = EntropyCalculator.CalculateForDistribution(distribution);
+            double empiricalEntropy = EntropyCalculator.CalculateForData(generatedData);
+
+            Assert.Equal(theoreticalEntropy, empiricalEntropy, 2);
+        }
+
+        [Fact]
+        public void GeneratorCalculatedEntropyEqualToTheoreticalEntropyTest()
+        {
+            int count = 1000000;
+            var data = new char[] { 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'D' };
+            var distribution = new EmpiricalDataDistribution<char>(data);
+            var randomSource = new DiscreteFiniteSetValueGenerator<char>(distribution, new SystemUniformRandomSource());
+            var sg = new SequenceGenerator<char>(randomSource);
+            var generatedData = sg.Generate(count).ToArray();
+
+            double theoreticalEntropy = EntropyCalculator.CalculateForDistribution(distribution);
+            double generatorEntropy = randomSource.TotalGeneratedEntropy;
+
+            Assert.Equal(theoreticalEntropy, generatorEntropy, 2);
+        }
+    }
+}
