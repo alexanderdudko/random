d5639312386f03fc9209d062e62ba0c6e4a6a9b7
Alexander <aleksandrsdudko@gmail.com>
2018-11-29 13:14:38 +0200

    Fixed entropy calculation code to properly deal with zero probability items in distribution

--- Generator/Distribution/Entropy/EntropyCalculator.cs
+++ Generator/Distribution/Entropy/EntropyCalculator.cs
@@ -9,7 +9,15 @@ namespace Generator.Distribution.Entropy
     {
         public static double CalculateForDistribution<T>(IDiscreteValueDistribution<T> distribution)
         {
-            return -distribution.Values.Sum(x => distribution.ValueProbability(x) * Math.Log(distribution.ValueProbability(x), 2));
+            double sum = 0;
+            foreach (var value in distribution.Values)
+            {
+                double valueP = distribution.ValueProbability(value);
+                if (valueP > 0)
+                    sum += valueP * Math.Log(valueP, 2);
+            }
+
+            return -sum;
         }
         public static double CalculateForData<T>(IEnumerable<T> data)
         {
