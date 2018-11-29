54b108f2c41d747070d79d19a12371880471b10c
Alexander <aleksandrsdudko@gmail.com>
2018-11-29 11:15:04 +0200

    Added parameter search feature.

---
+++ GeneratorCLI/Search/ParameterSearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneratorCLI.Search
+{
+    class ParameterSearch
+    {
+        public static double SearchParameterValue(Func<double, double> f, double targetY, double minX, double maxX, double precisionY, int maxStepsCount = 1000)
+        {
+            bool increasing = f(maxX) > f(minX);
+
+            int stepsCount = 0;
+            double currentX = minX;
+            while (stepsCount < maxStepsCount)
+            {
+                // Take middle point as a guess
+                currentX = (maxX + minX) / 2;
+
+                // Evaluate function value
+                double currentY = f(currentX);
+
+                // Check if target found
+                if (Math.Abs(currentY - targetY) <= precisionY) return currentX;
+
+                // Reduce search area
+                if (increasing && targetY >= currentY || !increasing && targetY < currentY)
+                    minX = currentX;
+                else
+                    maxX = currentX;
+
+                stepsCount++;
+            }
+            return currentX;
+        }
+    }
+}
---
+++ GeneratorCLIUnitTest/ParameterSearchTests.cs
@@ -0,0 +1,50 @@
+﻿using GeneratorCLI.Search;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GeneratorCLIUnitTest
+{
+    public class ParameterSearchTests
+    {
+        [Fact]
+        public void SearchIncreasingLinearFunctionTest()
+        {
+            Func<double, double> f = x => 5 * x + 9;
+            double searchTargetY = 15;
+            double minX = 0;
+            double maxX = 4;
+
+            double searchResult = ParameterSearch.SearchParameterValue(f, searchTargetY, minX, maxX, 0.001);
+
+            Assert.Equal(1.2, searchResult, 3);
+        }
+
+        [Fact]
+        public void SearchDecreasingLinearFunctionTest()
+        {
+            Func<double, double> f = x => -2 * x + 6;
+            double searchTargetY = 15;
+            double minX = -10;
+            double maxX = 10;
+
+            double searchResult = ParameterSearch.SearchParameterValue(f, searchTargetY, minX, maxX, 0.001);
+
+            Assert.Equal(-4.5, searchResult, 3);
+        }
+
+        [Fact]
+        public void SearchTargetOutsideBoudariesTest()
+        {
+            Func<double, double> f = x => 5 * x + 9;
+            double searchTargetY = 15;
+            double minX = 5;
+            double maxX = 10;
+
+            double searchResult = ParameterSearch.SearchParameterValue(f, searchTargetY, minX, maxX, 0.001);
+
+            Assert.Equal(minX, searchResult, 3);
+        }
+    }
+}
